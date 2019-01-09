9598e3715023a6f52c14db169bca37e9883f9a45
gayathribuddhika <39160904+gayathribuddhika@users.noreply.github.com>
2019-01-09 10:10:42 +0530

    Logical Operator
    Logical Opearator with If function

---
+++ LogicalOperator.java
@@ -0,0 +1,15 @@
+public class LogicalOperator {
+
+    
+    public static void main(String[] args) {
+        int sub1 = 45;
+        int sub2 = 25;
+        //&&--AND Operator,||--OR Operator
+        if((sub1>=35) &&  (sub2>=35)){
+            System.out.println("The condition is true");
+        }
+        else{
+            System.out.println("The condition is false");
+        }
+}
+}
